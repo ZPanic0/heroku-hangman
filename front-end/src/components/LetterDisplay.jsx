405d4799cac421b28ea3c8c111be14be5942dba4
jgregor <jamesgregor418@gmail.com>
2020-01-11 12:37:20 -0500

    Move letter statistics rendering to LetterDisplay in anticipation of win/loss statistics

---
+++ front-end/src/components/LetterDisplay.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import { Statistic, Segment, Grid } from 'semantic-ui-react'
+
+export default class LetterDisplay extends Component {
+    render() {
+        //Adding unrendered Statistic to avoid styling issue with first element of a statistic group
+        const rows = [<Statistic key='unrendered' style={{ display: 'none' }} />]
+        let sum = 0;
+
+        for (const [key, val] of Object.entries(this.props.statistics)) {
+            if (!'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.includes(key)) continue
+
+            rows.push(<Statistic key={key}>
+                <Statistic.Value>{val}</Statistic.Value>
+                <Statistic.Label>{key}</Statistic.Label>
+            </Statistic>)
+
+            sum += val
+        }
+
+        return <Segment raised>
+            <Grid>
+                <Grid.Row verticalAlign='middle'>
+                    <Grid.Column width={2}>
+                        <Statistic.Group size='small'>
+                            <Statistic>
+                                <Statistic.Value>{sum}</Statistic.Value>
+                                <Statistic.Label>Total</Statistic.Label>
+                            </Statistic>
+                        </Statistic.Group>
+                    </Grid.Column>
+                    <Grid.Column width={14}>
+                        <Statistic.Group widths='9' size='small'>
+                            {rows}
+                        </Statistic.Group>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+        </Segment>
+    }
+}
--- front-end/src/components/StatisticsBoard.jsx
+++ front-end/src/components/StatisticsBoard.jsx
@@ -1,37 +1,12 @@
 import React, { Component } from "react"
-import { Table } from 'semantic-ui-react'
+import LetterDisplay from "./LetterDisplay"
+
 
 export default class StatisticsBoard extends Component {
-
-    toTableRows(statistics) {
-        const rows = []
-
-        for (const [key, val] of Object.entries(statistics)) {
-            if (!'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.includes(key)) {continue}
-
-            rows.push(
-                <Table.Row key={key}>
-                    <Table.Cell>{key}</Table.Cell>
-                    <Table.Cell>{val}</Table.Cell>
-                </Table.Row>
-            )
-        }
-
-        return rows
-    }
-
     render() {
-        return <Table celled style={this.props.style}>
-            <Table.Header>
-                <Table.Row>
-                    <Table.HeaderCell>Letter</Table.HeaderCell>
-                    <Table.HeaderCell>Times Guessed (Globally)</Table.HeaderCell>
-                </Table.Row>
-            </Table.Header>
-
-            <Table.Body>
-                {this.toTableRows(this.props.letterStatistics)}
-            </Table.Body>
-        </Table>
+        return <div style={this.props.style}>
+            <h3>Individual Letter Statistics</h3>
+            <LetterDisplay statistics={this.props.letterStatistics} />
+        </div>
     }
 }
