0e466c2a51a636dbe235d814584558c811d3bfee
jgregor <jamesgregor418@gmail.com>
2020-01-12 20:42:53 -0500

    Remove debug logging from TopWinDisplay

--- front-end/src/components/TopWinDisplay.jsx
+++ front-end/src/components/TopWinDisplay.jsx
@@ -39,7 +39,6 @@ export default class TopWinDisplay extends Component {
     }
 
     render() {
-        console.log(this.state.statistics)
         return <Segment>
             <Dimmer inverted active={this.state.loading}>
                 <Loader>Loading</Loader>
